2fd5f5bc5bbe0f0f01a320ce197f5349dc69758d
CrisSal2 <163702291+CrisSal2@users.noreply.github.com>
2024-08-19 21:51:25 -0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,15 @@
-const Sequelize = require("sequelize");
-require("dotenv").config();
+let sequelize;
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST || "localhost",
-    dialect: "postgres",
-  }
-);
-
-module.exports = sequelize;
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+    },
+  );
+}
